174334868cf7bf829ce75296958007a523160cfb
Bastian-Guti <ba.guiterrezb@uocuc.cl>
2025-05-19 10:53:54 -0400

    PrimerAvance

---
+++ src/main/java/com/ecomarket_spa/ecomarket/model/TipoUsuario.java
@@ -0,0 +1,19 @@
+package com.ecomarket_spa.ecomarket.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+@Data
+@Entity
+public class TipoUsuario {
+@Id
+    @GeneratedValue
+    private long id;
+    
+    private String nombre;
+
+    @OneToMany(mappedBy = "tipoUsuario")
+    private List<> ;
+}
---
+++ src/main/java/com/ecomarket_spa/ecomarket/model/Vendedor.java
@@ -0,0 +1,5 @@
+package com.ecomarket_spa.ecomarket.model;
+
+public class Vendedor {
+
+}
