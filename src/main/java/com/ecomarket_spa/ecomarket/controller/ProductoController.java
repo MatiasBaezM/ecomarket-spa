c7e6760d396fa5e3bf7f8713569a113c45b1010d
Bastian-Guti <ba.gutierrezb@duocuc.cl>
2025-05-27 19:40:44 -0400

    modelos ordenados 1

--- src/main/java/com/ecomarket_spa/ecomarket/controller/ProductoController.java
+++ src/main/java/com/ecomarket_spa/ecomarket/controller/ProductoController.java
@@ -1,5 +1,56 @@
 package com.ecomarket_spa.ecomarket.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomarket_spa.ecomarket.model.Producto;
+import com.ecomarket_spa.ecomarket.services.ProductoService;
+
+
+@RestController
+@RequestMapping("/api/productos")
 public class ProductoController {
+
+private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    // Endpoint para obtenerproductos
+    @GetMapping
+    public List<Producto> getAllProductos() {
+        return productoService.getAllProductos();
+    }
+
+    // Endpoint para obtener productos por id
+    @GetMapping("/{id}")
+    public Producto getProductoById(@PathVariable Long id) {
+        return productoService.getProductoById(id);
+    }
+
+    // Endpoint para crear productos
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoService.createProducto(producto);
+    }
+
+    @PutMapping("/{id}")
+public Producto updateProducto(@PathVariable Long id, @RequestBody Producto producto) {
+    return productoService.updateProducto(id, producto);
+}
+    // Endpoint para eliminar un producto por id
+    @DeleteMapping("/{id}")
+    public void deleteProducto(@PathVariable Long id) {
+        productoService.deleteProducto(id);
+    }
 
 }
